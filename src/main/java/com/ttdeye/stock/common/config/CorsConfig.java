4832484c2536bea9a2c702e87af81f08224fdff2
zhangyongming <zhangyongming@newlink.com>
2022-04-28 16:04:03 +0800

    修改docker配置

---
+++ src/main/java/com/ttdeye/stock/common/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.ttdeye.stock.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/***
+ **@author 张永明
+ **@date 2022/4/28 16:01
+ ***/
+public class CorsConfig {
+
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        /* 是否允许请求带有验证信息 */
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+
+
+}
